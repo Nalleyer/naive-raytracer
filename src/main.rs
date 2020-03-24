9b272aa1f347dc1e0d6f4e4844269a09c456505f
Nalleyer <nalleyer@gmail.com>
2020-03-24 13:47:22 +0800

    add another sphere

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 extern crate image;
 
-use image::{DynamicImage, Rgba, RgbaImage, ImageBuffer};
 mod math;
 mod rendering;
 mod scene;
 
 use math::Point;
-use rendering::{Ray, cast_ray, render};
+use rendering::{cast_ray, render, Ray};
 use scene::{Color, Scene, Sphere};
 
 fn main() {
@@ -19,19 +18,34 @@ fn test_can_render_scene() {
         width: 800,
         height: 600,
         fov: 90.0,
-        items: vec![Box::new(Sphere {
-            center: Point {
-                x: 0.0,
-                y: 0.0,
-                z: -5.0,
-            },
-            radius: 1.0,
-            color: Color {
-                r: 0.4,
-                g: 1.0,
-                b: 0.4,
-            },
-        })],
+        items: vec![
+            Box::new(Sphere {
+                center: Point {
+                    x: 0.0,
+                    y: 0.0,
+                    z: -5.0,
+                },
+                radius: 1.0,
+                color: Color {
+                    r: 0.4,
+                    g: 1.0,
+                    b: 0.4,
+                },
+            }),
+            Box::new(Sphere {
+                center: Point {
+                    x: 2.0,
+                    y: 2.0,
+                    z: -7.5,
+                },
+                radius: 3.5,
+                color: Color {
+                    r: 0.7,
+                    g: 0.7,
+                    b: 1.0,
+                },
+            }),
+        ],
     };
 
     let img = render(&scene).to_rgb();
